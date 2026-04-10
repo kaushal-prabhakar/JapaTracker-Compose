55cadadbdbd05b20196aadd307954b3cfd1dc446
kaushalp <kaushalphrr@gmail.com>
2026-04-10 17:10:05 +0530

    chore: remove unused import.

--- app/src/main/java/com/kaushal/japacountercompose/ui/composables/WelcomeScreen.kt
+++ app/src/main/java/com/kaushal/japacountercompose/ui/composables/WelcomeScreen.kt
@@ -17,7 +17,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
-import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
