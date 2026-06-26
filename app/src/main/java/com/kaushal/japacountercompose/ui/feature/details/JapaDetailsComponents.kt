095fc6eb322d91fb720800b224724a1e4006b212
kaushalp <kaushalphrr@gmail.com>
2026-06-26 16:36:48 +0530

    fix: update button UI issue fix when keyboard is opened.

--- app/src/main/java/com/kaushal/japacountercompose/ui/feature/details/JapaDetailsComponents.kt
+++ app/src/main/java/com/kaushal/japacountercompose/ui/feature/details/JapaDetailsComponents.kt
@@ -17,8 +17,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
@@ -490,6 +492,7 @@ fun UpdateCountBottomSheetContent(
     Column(
         modifier = Modifier
             .fillMaxWidth()
+            .verticalScroll(rememberScrollState())
             .padding(start = 24.dp, end = 24.dp, bottom = 48.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
